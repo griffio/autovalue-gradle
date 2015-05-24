9484ea33f036e1ca033ce3ba7a7870041f94d208
griffio <griffio@users.noreply.github.com>
2015-05-24 17:42:59 +0100

    Cash Builder

--- src/main/java/griffio/Cash.java
+++ src/main/java/griffio/Cash.java
@@ -15,10 +15,24 @@ public abstract class Cash {
     Cash() {}
 
     public abstract Currency currency();
-    public abstract BigDecimal value();
+    public abstract BigDecimal amount();
 
-    public static Cash create(Currency currency, BigDecimal value) {
-        return new AutoValue_Cash(currency, value.setScale(2, BigDecimal.ROUND_HALF_UP));
+    public static Cash create(Currency currency, BigDecimal amount) {
+        return new AutoValue_Cash.Builder()
+            .currency(currency)
+            .amount(amount.setScale(2, BigDecimal.ROUND_HALF_UP))
+            .build();
+    }
+
+    public static Builder builder() {
+        return new AutoValue_Cash.Builder();
+    }
+
+    @AutoValue.Builder
+    abstract static class Builder {
+        abstract Builder currency(Currency currency);
+        abstract Builder amount(BigDecimal amount);
+        abstract Cash build();
     }
 
 }
